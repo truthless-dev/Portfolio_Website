92f336266d83b9e331fde4e4fb4ff94ecd1c3671
truthless-dev <truthless.deviant248@simplelogin.fr>
2024-08-25 21:03:24 -0700

    Add initial Django setup
    Break the usual `settings.py` module into multiple modules in a `settings`
    package in order to ease configuration in various contexts. Note that this
    means the `DJANGO_SETTINGS_MODULE` environment variable will need to be set
    in order for the app to work correctly.
    
    Further, store sensitive values in a file named `BASE_DIR / ".env"`. This
    file will need to be present and contain the following variables for the app
    to work correctly:
    
    * DJANGO_SECRET_KEY
    
    The production version of `.env` must contain these variables in addition to
    those listed above:
    
    * DJANGO_DB_NAME
    * DJANGO_DB_USER
    * DJANGO_DB_PASSWORD

---
+++ portfolio/settings/development.py
@@ -0,0 +1,11 @@
+from portfolio.settings.common import *  # noqa: F403
+
+
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # noqa: F405
+    }
+}
---
+++ portfolio/settings/production.py
@@ -0,0 +1,24 @@
+import os
+
+from portfolio.settings.common import *  # noqa: F403
+
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "712.235.50.46",
+    ".tylerchristie.dev",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ["DJANGO_DB_NAME"],
+        "USER": os.environ["DJANGO_DB_USER"],
+        "PASSWORD": os.environ["DJANGO_DB_PASSWORD"],
+        "HOST": "localhost",
+        "PORT": "",
+    }
+}
