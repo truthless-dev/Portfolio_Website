cd0c94790786bb37aa43ca00147bcd9dde55fb70
truthless-dev <truthless.deviant248@simplelogin.fr>
2024-08-26 11:18:51 -0700

    Condense the Braille index page
    Replace the previously verbose how-to notes with an accessibly
    collapsable region that will initially hide this information for a
    smoother user experience.
    
    Modify the base and meta templates to include Bootstrap and its plugins
    correctly. These are needed for the collapsing button on this page and
    will doubtless be used widely throughout the project later.
    
    Add unit tests for the Braille app.

--- braille/tests.py
+++ braille/tests.py
@@ -1,4 +1,12 @@
+from http import HTTPStatus
+
 from django.test import TestCase
+from django.urls import reverse
 
 
-# Create your tests here.
+class TestIndexView(TestCase):
+
+    def test_GET(self):
+        url = reverse("braille")
+        response = self.client.get(url, follow=True)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
